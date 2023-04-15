fcd3707f65674f4ed0828158d2971a70e3f31122
Conneroisu <conneroisu@outlook.com>
2023-04-15 01:49:51 -0500

    solved and refactoring

--- src/edu/iastate/cs228/hw4/MsgTree.java
+++ src/edu/iastate/cs228/hw4/MsgTree.java
@@ -4,45 +4,102 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/**
- * @author Kenneth Schueman
- * <p>
- * Com S 228 Fall 2022
- * Project 4: Archived Message Reconstruction
+/*
+ * @author Conner Ohnesorge
+ *         <p>
+ *         Com S 228 Spring 2023 Homework 4: Archived Message Reconstruction
+ *         </p>
  */
 
+/**
+ * A message tree is a binary tree that stores characters in the leaves. The
+ * internal nodes of the tree store the character '^'. The left subtree of a
+ * node with payload '^' corresponds to code 0, and the right subtree
+ * corresponds to code 1. The message tree is constructed from a string of
+ * characters.
+ */
 public class MsgTree {
 
-public char payloadChar;
-public MsgTree left;
-public MsgTree right;
-
-private static int staticCharIdx = 0;
-
-private static String encodeStr;
-private static String encodingStr;
+/**
+ * Static integer used to keep track of the index of the encoded string
+ */
+private static int staticCharacterIndex = 0;
+/**
+ * String used to store the encoded string
+ */
+private static String encodeString;
+/**
+ * String used to store the encoding string
+ */
+private static String encodingString;
+/**
+ * String used to store the file name
+ */
 private static String inputFileName;
+/**
+ * Character used to store the payload character
+ */
+public char payloadCharacter;
 
 /**
- * Takes .arch file, creates binary tree and then decodes and prints message
- *
- * @throws FileNotFoundException    if the file does not exist or is not found
+ * The Left MsgTree reference.
  */
-public static void main(String[] args) throws FileNotFoundException
-{
+public MsgTree left;
+/**
+ * The Right MsgTree reference.
+ */
+public MsgTree right;
+
+/**
+ * Constructor for a MsgTree object.A MsgTree object is created from a string of
+ * characters.
+ *
+ * @param EncodedStr the given string to be decoded
+ */
+public MsgTree(String EncodedStr) {
+    // initialize the payloadCharacter to the character at the current static character index.
+    this.payloadCharacter = EncodedStr.charAt(staticCharacterIndex);
+    // increment the static character index.
+    staticCharacterIndex++;
+    // initialize the left MsgTree reference to a new MsgTree object constructed with the character at the static character index.
+    this.left = new MsgTree(EncodedStr.charAt(staticCharacterIndex));
+    // if the left MsgTree reference is a leaf node, then set the left MsgTree reference to a new MsgTree object constructed with the encoded string.
+    if (this.left.payloadCharacter == '^') {
+        this.left = new MsgTree(EncodedStr);
+    }
+    // increment the static character index.
+    staticCharacterIndex++;
+    // initialize the right MsgTree reference to a new MsgTree object constructed with the character at the static character index.
+    this.right = new MsgTree(EncodedStr.charAt(staticCharacterIndex));
+    // if the right MsgTree reference is a leaf node, then set the right MsgTree reference to a new MsgTree object constructed with the encoded string.
+    if (this.right.payloadCharacter == '^') {
+        this.right = new MsgTree(EncodedStr);
+    }
+}
+
+/**
+ * Constructor for a single node MsgTree
+ *
+ * @param payloadCharacter char being set as payloadChar.
+ */
+public MsgTree(char payloadCharacter) {
+    this.payloadCharacter = payloadCharacter;
+}
+
+/**
+ * Ask user for file name and then decodes message by first taking the .arch
+ * file given,creates binary tree and then decodes and prints message
+ *
+ * @throws FileNotFoundException if the file does not exist or is not found
+ */
+public static void main(String[] args) throws FileNotFoundException {
     getFileName();
-
     buildCodeStrings();
-
-    MsgTree mainTree = new MsgTree(encodeStr);
-
+    MsgTree mainTree = new MsgTree(encodeString);
     System.out.println("Character\tCode: \n------------------------");
-
     printCodes(mainTree, "");
-
     System.out.println("------------------------\nMessage:");
-
-    decode(mainTree, encodingStr);
+    decode(mainTree, encodingString);
 }
 
 /**
@@ -50,10 +107,8 @@ public static void main(String[] args) throws FileNotFoundException
  *
  * @throws FileNotFoundException if the file does not exist or is not found
  */
-private static void getFileName() throws FileNotFoundException
-{
+private static void getFileName() throws FileNotFoundException {
     Scanner inputScanner = new Scanner(System.in);
-
     System.out.print("Please enter filename to decode: ");
     inputFileName = inputScanner.nextLine().trim();
 
@@ -61,95 +116,59 @@ private static void getFileName() throws FileNotFoundException
 }
 
 /**
- * Builds strings from file input
+ * Builds strings from the file input variable and sets them to the static
+ * variables encodeString and encodingString
  *
  * @throws FileNotFoundException if the file does not exist or is not found
  */
 private static void buildCodeStrings() throws FileNotFoundException {
 
+    // Create a input file object with the given file name.
     File inputFile = new File(inputFileName);
-
+    // Create a scanner object to read from the input file.
     Scanner fileScanner;
-
-    try
-    {
+    // Try to open the file. If the file does not exist, throw an exception.
+    try {
         fileScanner = new Scanner(inputFile);
-    }
-
-    catch (FileNotFoundException e)
-    {
+    } catch (FileNotFoundException e) {
         throw new FileNotFoundException(" File '" + inputFile + "' does not exist. ");
     }
-
-    encodeStr = fileScanner.nextLine();
+    encodeString = fileScanner.nextLine();
     String tempStr = fileScanner.nextLine();
-    encodingStr = "";
+    encodingString = "";
 
     for (int x = 0; x < tempStr.length(); x++) {
         if (tempStr.charAt(x) != '1' && tempStr.charAt(x) != '0') {
-            encodeStr += "\n";
-            encodeStr += tempStr;
-            encodingStr = fileScanner.nextLine();
+            encodeString += "\n";
+            encodeString += tempStr;
+            encodingString = fileScanner.nextLine();
             x = tempStr.length();
         } else {
-            encodingStr = tempStr;
+            encodingString = tempStr;
         }
     }
     fileScanner.close();
 }
 
 /**
- * Constructs a tree
- *
- * @param EncodedStr string to be decoded
- */
-public MsgTree(String EncodedStr)
-{
-    this.payloadChar = EncodedStr.charAt(staticCharIdx);
-
-    staticCharIdx++;
-
-    this.left = new MsgTree(EncodedStr.charAt(staticCharIdx));
-
-    if (this.left.payloadChar == '^')
-        this.left = new MsgTree(EncodedStr);
-
-    staticCharIdx++;
-
-    this.right = new MsgTree(EncodedStr.charAt(staticCharIdx));
-
-    if (this.right.payloadChar == '^')
-        this.right = new MsgTree(EncodedStr);
-}
-
-/**
- * Constructor for a single node MsgTree
- *
- * @param payloadChar char being set as payloadChar.
- */
-public MsgTree(char payloadChar)
-{
-    this.payloadChar = payloadChar;
-
-}
-
-/**
- * Prints values from binary tree and their binary codes
+ * Prints the codes for each character in the message tree.
  *
  * @param root    = root tree
  * @param codeMsg = binary code
  */
-public static void printCodes(MsgTree root, String codeMsg)
-{
-    if (root == null)
+public static void printCodes(MsgTree root, String codeMsg) {
+    // if the root is null, then return.
+    if (root == null) {
         return;
+    }
 
-    if (root.payloadChar != '^')
-    {
-        System.out.print(root.payloadChar + "\t\t");
+    // if the root is a leaf node, then print the payload character and the code message.
+    if (root.payloadCharacter != '^') {
+        System.out.print(root.payloadCharacter + "\t\t");
         System.out.println(codeMsg);
     }
 
+    // print the left and right subtree codes.
     printCodes(root.left, codeMsg + "0");
     printCodes(root.right, codeMsg + "1");
 }
@@ -157,11 +176,10 @@ public static void printCodes(MsgTree root, String codeMsg)
 /**
  * Decodes message using binary tree method
  *
- * @param codes a binary search tree given character codes.
- * @param message   = to be decoded
+ * @param codes   a binary search tree given character codes.
+ * @param message = to be decoded
  */
-public static void decode(MsgTree codes, String message)
-{
+public static void decode(MsgTree codes, String message) {
     MsgTree thisTree = codes;
 
     char curChar;
@@ -169,53 +187,39 @@ public static void decode(MsgTree codes, String message)
     int msgLength = message.length();
     int msgLastIndex = message.length() - 1;
 
-    String decodedMsg = "";
+    StringBuilder decodedMsg = new StringBuilder();
 
-    while (curCharIndex < msgLength)
-    {
+    while (curCharIndex < msgLength) {
         curChar = message.charAt(curCharIndex);
 
-        if (curChar == '0')
-        {
-            if (thisTree.left == null)
-            {
-                decodedMsg += thisTree.payloadChar;
+        if (curChar == '0') {
+            if (thisTree.left == null) {
+                decodedMsg.append(thisTree.payloadCharacter);
                 thisTree = codes;
-            }
-
-            else
-            {
+            } else {
                 thisTree = thisTree.left;
                 curCharIndex = curCharIndex + 1;
             }
-        }
-
-        else if (curChar == '1')
-        {
-            if (thisTree.right == null)
-            {
-                decodedMsg += thisTree.payloadChar;
+        } else if (curChar == '1') {
+            if (thisTree.right == null) {
+                decodedMsg.append(thisTree.payloadCharacter);
                 thisTree = codes;
-            }
-
-            else
-            {
+            } else {
                 thisTree = thisTree.right;
                 curCharIndex = curCharIndex + 1;
             }
         }
 
-        if (curCharIndex == msgLastIndex)
-        {
-            if (curChar == '0')
-            {
-                decodedMsg += thisTree.left.payloadChar;
+        if (curCharIndex == msgLastIndex) {
+            if (curChar == '0') {
+                assert thisTree.left != null;
+                decodedMsg.append(thisTree.left.payloadCharacter);
                 curCharIndex = curCharIndex + 1;
             }
 
-            if (curChar == '1')
-            {
-                decodedMsg += thisTree.right.payloadChar;
+            if (curChar == '1') {
+                assert thisTree.right != null;
+                decodedMsg.append(thisTree.right.payloadCharacter);
                 curCharIndex = curCharIndex + 1;
             }
         }
---
+++ src/edu/iastate/cs228/hw4/TestMsgTree.java
@@ -0,0 +1,21 @@
+package edu.iastate.cs228.hw4;
+import org.junit.Test;
+
+/*
+  * @author Conner Ohnesorge
+  *        <p>
+  *        JUNIT test for Com S 228 Spring 2023 Homework 4: Archived Message Reconstruction
+  *       </p>
+ */
+public class TestMsgTree {
+
+
+@Test
+public void testMsgTree () {
+
+}
+
+
+
+
+}
